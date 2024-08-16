a1c0ab8222cce864d83cd2e52b827fc24459f856
mugaber <mohammedgaber273@GMAIL.COM>
2024-08-16 21:06:34 +0300

    feat: integrate apollo client and get the data from contentful

--- packages/my-app/src/App.tsx
+++ packages/my-app/src/App.tsx
@@ -1,13 +1,36 @@
+import { gql, useQuery } from '@apollo/client';
 import './App.css'
 import { SurveyComponent, defineCustomElements } from 'react-library';
 
 defineCustomElements()
 
+const ASSESSMENT_QUERY = gql`
+  {
+    assessmentCollection {
+      items {
+        name
+        slug
+        questions
+        intro {
+          json
+        }
+          resultsIntro {
+            json
+          }
+      }
+    }
+  }
+`
+
 function App() {
+  const { loading, error, data } = useQuery(ASSESSMENT_QUERY)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :{error.message}</p>
 
   return (
     <div className='my-app'>
-      <SurveyComponent surveyData={[]} />
+      <SurveyComponent surveyData={data} />
     </div>
   )
 }
