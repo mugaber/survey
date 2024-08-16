32df5fb7cc83c81230da1120066c3dc533cdad5b
mugaber <mohammedgaber273@GMAIL.COM>
2024-08-16 21:30:57 +0300

    feat: create survey header with actual data utilizing contentful rich text renderer

--- packages/stencil-library/src/components/survey-component/survey-component.tsx
+++ packages/stencil-library/src/components/survey-component/survey-component.tsx
@@ -1,4 +1,11 @@
 import { Component, Host, Prop, h } from '@stencil/core';
+import mockSurveyData from '../../data/surveyData'
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+import {
+  getSurveyData,
+  getSurveyQuestions,
+  truncateText
+} from '../../utils/utils';
 
 @Component({
   tag: 'survey-component',
@@ -8,17 +15,20 @@ import { Component, Host, Prop, h } from '@stencil/core';
 export class SurveyComponent {
   @Prop() surveyData: any;
 
-  connectedCallback() {
-    console.log('Survey component connected')
-    console.log({ surveyData: this.surveyData })
-  }
-
   render() {
+    const survey = getSurveyData(mockSurveyData)
+    const allQuestions = getSurveyQuestions(survey)
+    const estimatedTime = allQuestions?.length * .5
+    const introHtmlString = documentToHtmlString(survey?.intro?.json)
+    const truncatedIntro = truncateText(introHtmlString, 200)
+
     return (
       <Host>
-        <div>
-          <h1>Survey component</h1>
-          <p>This is a survey component made with stencil and react.</p>
+        <div class="header">
+          <p class="label">assessment</p>
+          <h1>{survey?.name}</h1>
+          <p class="timer">Takes only {estimatedTime} minutes</p>
+          <div class="intro-container" innerHTML={truncatedIntro} />
         </div>
       </Host>
     )
--- packages/stencil-library/src/utils/utils.ts
+++ packages/stencil-library/src/utils/utils.ts
@@ -1,3 +1,24 @@
 export function format(first?: string, middle?: string, last?: string): string {
   return (first || '') + (middle ? ` ${middle}` : '') + (last ? ` ${last}` : '');
 }
+
+export function getSurveyData(rawData) {
+  if (!rawData) return null
+
+  const { assessmentCollection } = rawData || {}
+  const { items } = assessmentCollection || {}
+  const survey = items?.[0]
+  return survey
+}
+
+export function getSurveyQuestions(survey) {
+  if (!survey) return null
+  const questions = survey.questions.pages.map(
+    page => page.elements
+  )?.flat()
+  return questions
+}
+
+export function truncateText(text, length) {
+  return text.length > length ? `${text.substring(0, length)}...` : text
+}
